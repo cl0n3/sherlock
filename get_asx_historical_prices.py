20ce7ad02487f9661c4dac995e189b539d076f3c
rohan <rohan@cl0n3.net>
2018-06-16 09:46:36 +0000

    added parser for trades and fixed bugs with the asx downloader

--- get_asx_historical_prices.py
+++ get_asx_historical_prices.py
@@ -15,25 +15,39 @@ import csv
 #jhttplib.debuglevel = 1
 #httplib.HTTPConnection.debuglevel = 1
 
-# Try to connect
+def connect_db():
+    try:
+        conn=psycopg2.connect("dbname='vagrant' host='localhost' user='vagrant' password='password'")
+        return conn
+    except Exception as e:
+        print(e)
+        print "I am unable to connect to the database."
 
-try:
-    conn=psycopg2.connect("dbname='vagrant' host='localhost' user='vagrant' password='password'")
-except Exception as e:
-    print(e)
-    print "I am unable to connect to the database."
-    
-cur = conn.cursor()
-try:
-    cur.execute("""select symbol from trade group by symbol""")
-except Exception as e:
-    print "can't select symbols"
+def get_start_date(conn):
+    try:
+        cur = conn.cursor()
+        cur.execute('select min(time) from trade')
+        res = cur.fetchall()
+        d = res[0][0]
+        print 'start date: %s'%d
+        return d
+    except Exception as e:
+        print(e)
 
-results = cur.fetchall()
-symbols = []
-for r in results:
-    symbols.append(r[0])
-print str(symbols)
+def get_trade_codes(conn):
+    cur = conn.cursor()
+    try:
+        cur.execute("""select symbol from trade group by symbol""")
+        results = cur.fetchall()
+        symbols = []
+        for r in results:
+            symbols.append(r[0])
+
+        print str(symbols)
+        return symbols
+
+    except Exception as e:
+        print "can't select symbols"
 
 def next_weekday(d, weekday):
     days_ahead = weekday - d.weekday()
@@ -41,37 +55,118 @@ def next_weekday(d, weekday):
         days_ahead += 7
     return d + datetime.timedelta(days_ahead)
 
-# get start date argument 
-if len(sys.argv) == 2:
-    start_date = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d')
+def write_snapshot(conn, code, open, high, low, close, turnover, date):
+    try:
+        cur = conn.cursor()
+        cur.execute("""
+        insert into price_snapshot(symbol, open, high, low, close, turnover, date) 
+               values (%s, %s, %s, %s, %s, %s, %s)
+        """, (code, open, high, low, close, turnover, date))
+        conn.commit()
+        print 'wrote snapshot %s %s %s'%(date, code, close)
+    except psycopg2.IntegrityError as e:
+        conn.rollback()
 
-# find the next friday (data is grouped into batches dated the friday of the week)
-# iterate by fridays until past today.
-next_friday = next_weekday(start_date, 4)
-today = datetime.datetime.now()
+hist_arch = None
+def get_hist_arch():
+   
+    global hist_arch
+    if hist_arch is None:
+        weekurl = 'https://www.asxhistoricaldata.com/data/2013-2016.zip'
+        headers = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(weekurl, headers=headers)
+        if (r.status_code == 200):
+            hist_arch = ZipFile(StringIO(r.content))
+    
+        print 'retrieved 2013-2016 archive'
+    return hist_arch
 
-def getdata2(url):
-    # need to spoof a browser as the site rejects python user-agent.
-    headers = {'user-agent': 'Mozilla/5.0'}
-    r = requests.get(url, headers=headers)
-    if (r.status_code == 200):
-        z = ZipFile(StringIO(r.content))
-        for i in z.infolist():
-            if (i.filename.endswith('txt')):
-                f = z.open(i.filename)
-                reader = csv.DictReader(f, ['symbol','date','open','high','low','close','turnover'])
-                for row in reader:
-                    if (row['symbol'] in symbols):
-                        try:
-                            cur.execute("""
-                            insert into price_snapshot(symbol, open, high, low, close, turnover, date) values (%s, %s, %s, %s, %s, %s, %s)
-                            """, (row['symbol'], row['open'], row['high'], row['low'], row['close'], row['turnover'], datetime.datetime.strptime(row['date'], '%Y%m%d')))
-                            conn.commit()
-                        except psycopg2.IntegrityError as e:
-                            conn.rollback()
+h12017_arch = None
+def get_h12017_arch():
+    global h12017_arch
+    if h12017_arch is None:
+        weekurl = 'https://www.asxhistoricaldata.com/data/2017jan-june.zip'
+        headers = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(weekurl, headers=headers)
+        if (r.status_code == 200):
+            h12017_arch = ZipFile(StringIO(r.content))
+        print 'retrieved H1 2017 archive'
+    return h12017_arch
 
+h22017_arch = None
+def get_h22017_arch():
+    global h22017_arch
+    if h22017_arch is None:
+        weekurl = 'https://www.asxhistoricaldata.com/data/2017july-december.zip'
+        headers = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(weekurl, headers=headers)
+        if (r.status_code == 200):
+            h22017_arch = ZipFile(StringIO(r.content))
+        print 'retrieved H2 2017 archive'
+    return h22017_arch
 
-while (next_friday < today):
-    weekurl = next_friday.strftime('https://www.asxhistoricaldata.com/data/week%Y%m%d.zip')
-    getdata2(weekurl)
-    next_friday += datetime.timedelta(days=7)
+def get_week_arch(date):
+
+    # find the next friday (data is grouped into batches dated the friday of the week)
+    status = 404
+    next_friday = date
+    cnt = 0;
+    while status != 200 and cnt < 7:
+        weekurl = next_friday.strftime('https://www.asxhistoricaldata.com/data/week%Y%m%d.zip')
+        headers = {'user-agent': 'Mozilla/5.0'}
+        r = requests.get(weekurl, headers=headers)
+        status = r.status_code
+        print '%s -> %s'%(weekurl,status)
+        next_friday = next_friday + datetime.timedelta(days=1)
+        cnt += 1
+        
+    print 'retrieved data for week %s'%str(next_friday)
+    try:
+        return ZipFile(StringIO(r.content))
+    except Exception as e:
+        print(e)
+    return None
+
+def get_asx_data_for(date, symbols, conn):
+    
+    if date < datetime.datetime(2017, 1,1):
+        z = get_hist_arch()
+    elif date < datetime.datetime(2017, 7, 1):
+        z = get_h12017_arch()
+    elif date < datetime.datetime(2018, 1, 1):
+        z = get_h22017_arch()
+    else:
+        z = get_week_arch(date)
+    
+    if z is None:
+        return
+
+    filename = datetime.datetime.strftime(date, '%Y%m%d')
+    for i in z.infolist():
+        if (filename in i.filename):
+            f = z.open(i.filename)
+            reader = csv.DictReader(f, ['symbol','date','open','high','low','close','turnover'])
+            for row in reader:
+                if (row['symbol'] in symbols):
+                    write_snapshot(conn,
+                        row['symbol'], 
+                        row['open'], 
+                        row['high'], 
+                        row['low'], 
+                        row['close'], 
+                        row['turnover'], 
+                        datetime.datetime.strptime(row['date'], '%Y%m%d'))
+
+with connect_db() as conn:
+    # get start date argument 
+    if len(sys.argv) == 2:
+        start_date = datetime.datetime.strptime(sys.argv[1], '%Y-%m-%d')
+    else:
+        start_date = get_start_date(conn)
+
+    symbols = get_trade_codes(conn)
+    today = datetime.datetime.now()
+    while (start_date < today):
+        if start_date.weekday() < 5:
+            get_asx_data_for(start_date, symbols, conn)
+        start_date += datetime.timedelta(days=1)
